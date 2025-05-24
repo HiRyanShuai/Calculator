e8f2cbb37c24f9fa59779938924ae24f2f30c8ba
Ryan Shuai <141750879+HiRyanShuai@users.noreply.github.com>
2025-05-24 15:42:51 +0930

    complete number and ac/back buttons, especially situations of starting with  0 and ., and empty screen

--- calculator.js
+++ calculator.js
@@ -8,8 +8,10 @@ const ac = document.querySelector('#ac');
 const back = document.querySelector('#back');
 
 numbers.forEach(b=>b.addEventListener('click',(e)=>{
-    if( !(b.id === '.' && show.includes('.') || b.id === '0' && show === '0') )
+    if( !(b.id === '.' && show.includes('.')) )
         show += b.id;
+    if( show[0] ==='0' && show[1] !== '.')
+        show = show[1];
     screen.textContent = show;
 }));
 
@@ -20,5 +22,7 @@ ac.addEventListener('click', (e)=>{
 
 back.addEventListener('click', (e)=>{
     show = show.slice(0, -1);
+    if( show.length === 0) 
+        show = '0';
     screen.textContent = show;
 });
